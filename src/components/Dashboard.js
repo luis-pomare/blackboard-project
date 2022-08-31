88ffe0740a43e4210af0948a54547423f238f03c
unknown <luispomare92@gmail.com>
2022-08-31 13:34:23 -0500

    Add dashboard routes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Development from './main/Development';
 import Design from './main/Design';
 import Marketing from './main/Marketing';
@@ -7,10 +8,14 @@ import Tasks from './main/Tasks';
 export default function Dashboard() {
   return (
     <main>
-      <Development />
-      <Design />
-      <Marketing />
-      <Tasks />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/development' element={<Development />} />
+          <Route path='/design' element={<Design />} />
+          <Route path='/marketing' element={<Marketing />} />
+          <Route path='/' element={<Tasks />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
