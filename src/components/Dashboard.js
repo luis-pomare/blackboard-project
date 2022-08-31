2dab03188416ed1941c75d6b18aa479a28d28072
unknown <luispomare92@gmail.com>
2022-08-31 13:19:18 -0500

    Add basic main components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Development from './main/Development';
+import Design from './main/Design';
+import Marketing from './main/Marketing';
 
 export default function Dashboard() {
-  return <main>main</main>;
+  return (
+    <main>
+      <Development />
+      <Design />
+      <Marketing />
+    </main>
+  );
 }
