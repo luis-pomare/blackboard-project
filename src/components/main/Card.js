bf08e443559314c24853c9556a3cec998eb2bee5
unknown <luispomare92@gmail.com>
2022-09-01 16:41:28 -0500

    Fix card style

--- src/components/main/Card.js
+++ src/components/main/Card.js
@@ -4,22 +4,22 @@ import '../../styles/Card.css';
 export default function card() {
   return (
     <section className='card'>
-      <h2>Next task for team</h2>
+      <h2>Next task for the team</h2>
       <div className='cardContent'>
         <span>
           <h3>Todo:</h3>
           <p></p>
         </span>
         <span>
-          <h3>Time</h3>
+          <h3>Time:</h3>
           <p>hours</p>
         </span>
         <span>
-          <h3>Deadline</h3>
+          <h3>Deadline:</h3>
           <p></p>
         </span>
         <span>
-          <h3>Team</h3>
+          <h3>Team:</h3>
           <p></p>
         </span>
       </div>
