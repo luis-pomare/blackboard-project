dbcfb1272300ae5708dc64c6be38c59f7f5d8e0f
unknown <luispomare92@gmail.com>
2022-09-01 19:04:28 -0500

    Add tasks form

--- src/components/main/Tasks.js
+++ src/components/main/Tasks.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
 export default function Tasks(props) {
-  const { setState, state } = props;
+  // const { setState, state } = props;
 
-  const handleClick = () => {
-    setState({
-      ...state,
-      development: { todo: 'update', time: 'now', deadline: 'no' },
-    });
-  };
+  // // const handleSubmit = () => {
+  // //   setState({
+  // //     ...state,
+  // //     development: { todo: 'update', time: 'now', deadline: 'no' },
+  // //   });
+  // // };
 
   return (
     <div>
-      <button onClick={handleClick} type='button'>
-        set
-      </button>
+      <form>
+        <h2>Assign a task</h2>
+        <label for='todo'>Task to-do:</label>
+        <input type='text' id='todo' name='fname' maxlength='30' required />
+        <br />
+        <label for='time'>Time (between 1 and 15 hours):</label>
+        <input type='number' id='time' name='time' min='1' max='15' required />
+        <br />
+        <label for='deadline'>Deadline:</label>
+        <select name='deadline' id='deadline'>
+          <option value='none'>No deadline</option>
+          <option value='oneWeek'>1 week</option>
+          <option value='twoWeeks'>2 weeks</option>
+          <option value='threeWeeks'>3 weeks</option>
+        </select>
+        <br />
+        <label for='team'>Team:</label>
+        <select name='team' id='team' required>
+          <option value=''>None</option>
+          <option value='development'>Development</option>
+          <option value='design'>Design</option>
+          <option value='marketing'>Marketing</option>
+        </select>
+        <br />
+        <input type='submit' value='submit' />
+      </form>
     </div>
   );
 }
